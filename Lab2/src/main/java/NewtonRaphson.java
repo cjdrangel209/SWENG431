ee73ce4bc7e88237ba451766132616e1eb5d5b49
cjdrangel209 <cjdrangel209@gmail.com>
2022-01-20 09:09:49 -0500

    added functions

--- Lab2/src/main/java/NewtonRaphson.java
+++ Lab2/src/main/java/NewtonRaphson.java
@@ -5,8 +5,34 @@
 
 /**
  *
- * @author Cory
+ * @author Cory Drangel
  */
 public class NewtonRaphson {
+    int iteration = 0;
     
+    public double function(double x_0){
+        double val;
+        
+        val = 9*Math.pow(x_0, 6) - 7*Math.pow(x_0, 5) + 0*Math.pow(x_0, 4) - 
+                8*Math.pow(x_0, 3) + 7*Math.pow(x_0, 2) - 2*x_0 + 3;
+        
+        return val;
+    }
+    
+    public double derivedFunction(double x_0){
+        double val;
+        
+        val = 54*Math.pow(x_0, 5) - 35*Math.pow(x_0, 4) + 0*Math.pow(x_0, 3) -
+                24*Math.pow(x_0, 2) + 14*x_0 - 2;
+        
+        return val;
+    }
+    
+    public double nextNumber(double x_0){
+        double val;
+        
+        val = x_0 - (function(x_0)/derivedFunction(x_0));
+        
+        return val;
+    }
 }
