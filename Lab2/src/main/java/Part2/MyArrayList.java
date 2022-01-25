21d258dd387ad7079b6149cf01c1cbb285533c48
Noah Spott <66170809+noahspott@users.noreply.github.com>
2022-01-24 19:06:25 -0500

    Added various assert statements as well as comments return messages identifying the problem.
    In the constructor, the assertions are always true. Do you think we should include them anyway?

--- Lab2/src/main/java/Part2/MyArrayList.java
+++ Lab2/src/main/java/Part2/MyArrayList.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 /**
  *
  * @author Cory Drangel
+ * @author Noah Spott
  */
 
 // *** MyArrayList is limited to 100 elements. ***
@@ -15,11 +16,18 @@ public class MyArrayList<E> extends ArrayList<E> {
     
     public MyArrayList() {
         //assert postcondition
+
+        // NOTE: These are always true -- should we include them anyway??
+        assert al != null       : "The MyArrayList object == null.";
+        assert al.size() == 0   : "The size of the MyArrayList object == 0";
     }
     
     @Override
     public int size(){
         //assert postcondition
+        assert al != null       : "The MyArrayList object == null.";
+        assert al.size() <= no  : "The size of the MyArrayList object is > " + no;
+
         //code
         return al.size();
     }
@@ -27,41 +35,56 @@ public class MyArrayList<E> extends ArrayList<E> {
     //Insert e as a new first element to mal
     public void insertFirst(E e){
         //assert precondition
-        assert al.size() <= no;
+        assert e != null        : "Inserted element == null";
+        assert al.size() <= no  : "The size of the MyArrayList object is > " + no;
+
         //code
         al.add(0, e);
+
         //assert postcondition
-        assert al != null;
+        assert al != null       : "The MyArrayList object == null.";
+        assert al.get(0) == e   : "The first element != " + e;
     }
     
     //Insert e as a new last element
     public void insertLast(E e){
         //assert precondition
-        assert al.size() <= no;
+        assert e != null        : "Inserted element == null";
+        assert al.size() <= no  : "The size of the MyArrayList object is > " + no;
+
         //code
-        al.add(al.size(),e);
+        al.add(al.size() - 1, e);
+
         //assert postcondition
-        assert al != null;
+        assert al != null                   : "The MyArrayList object == null.";
+        assert al.get(al.size() - 1) == e   : "The last element of the MyArrayList object != " + e;
     }
     
     //Delete my first element
     public void deleteFirst(){
         //assert precondition
-        assert al!= null;
+        assert al != null           : "The MyArrayList object == null.";
+        assert al.get(0) != null    : "First element doesn't exists.";
+
         //code
         al.remove(0);
+
         //assert postcondition
-        assert al.size() < no;
+        assert al.size() <= no      : "The size of the MyArrayList object is > " + no;
     }
     
     //Delete my last element
     public void deleteLast(){
         //assert precondition
-        assert al != null;
+        assert al != null                       : "The MyArrayList object == null.";
+        assert al.get(al.size() - 1) != null    : "Last element doesn't exists.";
+
         //code
         al.remove(al.size() - 1);
+
         //assert postcondition
-        assert al.size() < no;
+        assert al != null           : "The MyArrayList object == null.";
+        assert al.size() < no       : "The size of the MyArrayList object is > " + no;
     }
     
     public void show(){
